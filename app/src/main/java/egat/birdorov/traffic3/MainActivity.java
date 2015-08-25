8c5eb71777bf35464a32d4e43c1037d11e17ecab
Birdorov <brs_fun@hotmail,com>
2015-08-25 15:19:08 +0700

    Button Controller Success

--- app/src/main/java/egat/birdorov/traffic3/MainActivity.java
+++ app/src/main/java/egat/birdorov/traffic3/MainActivity.java
@@ -1,16 +1,61 @@
 package egat.birdorov.traffic3;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView trafficListView;
+    private Button AboutMEButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind Widget
+        bindWidget();
+
+        // Button Controller
+        buttonController();
+
+    }   // Main Method
+
+    private void buttonController() {
+        AboutMEButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.phonton2);
+                buttonMediaPlayer.start();
+
+                // Internet to Webview
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://www.egat.co.th/index.php?option=com_content&view=article&id=346&Itemid=209"));
+                startActivity(objIntent);
+                
+            }   // event
+        });
+
+
+    }   // buttonController
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        AboutMEButton = (Button) findViewById(R.id.button);
+
     }
 
     @Override
@@ -34,4 +79,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
